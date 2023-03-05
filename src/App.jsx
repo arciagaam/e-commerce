b2bbe39a0ae428ff5d4e17bcb4d3d5492f6702b6
Miguel <miguelarciagaa@gmail.com>
2023-03-06 00:44:25 +0800

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,22 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import ShopLanding from './pages/ShopLanding';
 import ProductsList from './pages/ProductsList';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div className='font-display'>
-      <ul className='flex flex-row w-full bg-slate-300 py-3'>
-        <li>A</li>
-        <li>b</li>
-        <li>c</li>
-        <li>d</li>
-      </ul>
+
       <Routes>
-        <Route index path='/' element={<Home />} />
-        <Route index path='/shop' element={<ShopLanding />} />
-        <Route index path='/products' element={<ProductsList />} />
+
+        <Route path='/' element={<Navbar/>}>
+          <Route index element={<Home />} />
+          <Route path='shop' element={<ShopLanding />} />
+          <Route path='products' element={<ProductsList />} />
+        </Route>
+
+        
+        <Route path="*" element={<div>Page not Found!</div>}/>
       </Routes>
     </div>
   )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { NavLink, Outlet } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <>
+
+    <div className="flex flex-row justify-between items-center px-5 py-5">
+
+        <NavLink to='/' className='text-2xl'>
+            Sinagtala
+        </NavLink>
+
+        <div className="flex flex-row gap-5 text-base">
+            <NavLink to='collections'>
+               Collections
+            </NavLink>
+            <NavLink to='shop'>
+               Shop
+            </NavLink>
+            <NavLink to='about'>
+               About
+            </NavLink>
+            <NavLink to='profile'>
+               Profile
+            </NavLink>
+        </div>
+
+    </div>
+    <Outlet/>
+    </>
+
+  )
+}
+
+export default Navbar
