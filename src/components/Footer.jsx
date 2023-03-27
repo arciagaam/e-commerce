e4e8c9ae89d8091c1de45ce2124f1acd1521a7ba
Paul Caabay <95135206+plysh00@users.noreply.github.com>
2023-03-28 04:10:11 +0800

    show order status in profile orders

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="flex flex-row items-center justify-center min-h-[20vh] border-t border-accent-light bg-accent-light">
-            <div className="flex flex-1 flex-col">
+        <div className="flex flex-col items-center justify-center min-h-[20vh] border-t border-accent-light bg-accent-light gap-2">
+            <div className="flex flex-col items-center">
+                <p className='text-3xl font-semibold'>Sinagtala.ph</p>
+            </div>
+            <div className="flex flex-col items-center">
+                <p className='text-lg font-medium'>Sampaloc, Manila</p>
+            </div>
+            <div className="flex flex-row items-center gap-2">
+                <a target="_blank" href="https://www.facebook.com/Sinagtala.flwr"><box-icon name='facebook-square' size='md' type='logo' color='#df687d' ></box-icon></a>
+                <a target="_blank" href="https://www.instagram.com/sinag.talaph/"><box-icon name='instagram-alt' size='md' type='logo' color='#df687d' ></box-icon></a>
             </div>
         </div>
     )
--- src/pages/user/ShowProfileOrder.jsx
+++ src/pages/user/ShowProfileOrder.jsx
@@ -26,6 +26,11 @@ const ShowProfileOrder = () => {
     const [isDisplay, setIsDisplay] = useState(false);
     const [totalPrice, setTotalPrice] = useState(0);
     const [orderStatus, setOrderStatus] = useState('');
+    const [orderData, setOrderData] = useState('');
+    const [proceed, setProceed] = useState(false)
+    const [message, setMessage] = useState('')
+    const [status, setStatus] = useState(0)
+    const [textStatus, setTextStatus] = useState('');
 
     const callbackTotal = (totalPrice) => {
         setTotalPrice((prevTotal) => prevTotal + totalPrice);
@@ -62,6 +67,28 @@ const ShowProfileOrder = () => {
             }
         }
 
+        const getOrderData = async () => {
+            const docRef = doc(db, 'orders', params.id)
+            const docSnap = await getDoc(docRef);
+
+            if (docSnap.exists()) {
+                setMessage('');
+                setOrderData({ ...docSnap.data(), id: docSnap.id });
+                switch (docSnap.data().status) {
+                    case 'placed': setStatus(0); setTextStatus('Your order is placed.'); break;
+                    case 'packed': setStatus(1); setTextStatus('Your order is packed and is ready to be delivered.'); break;
+                    case 'shipped': setStatus(2); setTextStatus('Your order is on its way!'); break;
+                    case 'delivered': setStatus(3); setTextStatus('Package has been delivered.'); break;
+                }
+
+                setProceed(true);
+            } else {
+                setMessage('Order not found.');
+            }
+        }
+
+        params.id && getOrderData();
+
         getDetails();
     }, [])
 
@@ -72,6 +99,47 @@ const ShowProfileOrder = () => {
                     <button onClick={() => { navigate('../') }} className="self-center border border-accent-dark h-[30px] w-[30px] rounded-lg hover:bg-accent-default hover:text-white hover:font-bold hover:border-accent-default transition-all duration-100">{'<'}</button>
                 </div>
                 <p className='bg-accent-default text-white py-2 px-3 rounded-md self-start'>Order ID: {params.id}</p>
+            </div>
+
+            <div className='flex flex-col items-center justify-center  border-b-2'>
+
+                {
+                    proceed &&
+                    <div className="flex flex-col gap-14">
+                        <div className="flex flex-row items-center ">
+                            <div className="relative flex flex-col items-center">
+                                <div className={`flex flex-col items-center justify-center border-4 p-2 border-accent-default bg-accent-default rounded-full`}>
+                                    <box-icon name='file' type='solid' size='40px' color="#fff"></box-icon>
+                                </div>
+                                <p className='absolute bottom-[-50%] font-bold text-accent-default'>Placed</p>
+                            </div>
+                            <div className={`min-h-[5px] w-[50px] bg-accent-dark ${status >= 1 && 'bg-accent-default'}`}></div>
+                            <div className="relative flex flex-col items-center">
+                                <div className={`flex flex-col items-center justify-center border-4 p-2 border-accent-dark rounded-full ${status >= 1 && 'bg-accent-default border-accent-default'}`}>
+                                    <box-icon name='package' type='solid' size='40px' color={status >= 1 ? '#fff' : '#3B0C15'}></box-icon>
+                                </div>
+                                <p className={`absolute bottom-[-50%] ${status >= 1 && 'text-accent-default font-bold'}`}>Packed</p>
+                            </div>
+                            <div className={`min-h-[5px] w-[50px] bg-accent-dark ${status >= 2 && 'bg-accent-default'}`}></div>
+                            <div className="relative flex flex-col items-center">
+                                <div className={`flex flex-col items-center justify-center border-4 p-2 border-accent-dark rounded-full ${status >= 2 && 'bg-accent-default border-accent-default'}`}>
+                                    <box-icon name='truck' type='solid' size='40px' color={status >= 2 ? '#fff' : '#3B0C15'}></box-icon>
+                                </div>
+                                <p className={`absolute bottom-[-50%] ${status >= 2 && 'text-accent-default font-bold'}`}>Shipped</p>
+                            </div>
+                            <div className={`min-h-[5px] w-[50px] bg-accent-dark ${status >= 3 && 'bg-accent-default'}`}></div>
+                            <div className="relative flex flex-col items-center">
+                                <div className={`flex flex-col items-center justify-center border-4 p-2 border-accent-dark rounded-full ${status >= 3 && 'bg-accent-default border-accent-default'}`}>
+                                    <box-icon name='check' size='40px' color={status >= 3 ? '#fff' : '#3B0C15'}></box-icon>
+                                </div>
+                                <p className={`absolute bottom-[-50%] ${status >= 3 && 'text-accent-default font-bold'}`}>Delivered</p>
+                            </div>
+                        </div>
+
+                        <p className="flex self-center text-lg">{textStatus}</p>
+                    </div>
+                }
+
             </div>
 
             <div className="flex flex-row gap-10 pb-20">
